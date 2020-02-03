4e11b54e5256814cb882f7cd12a445f32f01b65b
Irsute <fred.then@gmail.com>
2020-02-03 20:38:35 +0100

    1st commit for sudoku resolver exercice

---
+++ README.md
@@ -0,0 +1,13 @@
+##learning repo
+
+### sudoku resolv ###
+````yaml
+langage: pyhton 3.6
+description:
+  resolve a sudoku array by filling the "0" value wihtin the array
+  In command line interface , the array is given as unique variable in the code
+# TODO:
+  make it with a graphic interface
+  put the avaibality to generate a grid
+  let a user try to fill it (
+  let the possibilty to the user to let the computer fnish
---
+++ sudoku_resolv/main.py
@@ -0,0 +1,61 @@
+from os import system
+
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+
+# function to print the board
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - -")
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print("| ", end="")
+            if j == 8:
+                print(str(bo[i][j]))
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+# function to retrieve case @ 0
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return i, j  # row, column
+
+
+# validate a value
+def valid(bo, num, pos):
+    # check value on the same row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num:
+            return False
+
+    # check value on the same column
+    for i in range(len(bo[0])):
+        if bo[i][pos[1]] == num:
+            return False
+
+    # check value on the same sub square
+    x = pos[0] // 3
+    y = pos[1] // 3
+
+    for i in range(len(bo[0])):
+        for j in range(len(bo[0])):
+            if i // 3 == x and j // 3 == y:
+                if bo[i][j] == num:
+                    return False
+
+
+print_board(board)
